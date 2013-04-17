63d086293b09780e96c85777a316648da08e6a90
vkravcov <vickravcov@gmail.com>
2013-04-17 20:29:26 +0400

    Залил сорцы

---
+++ PictureDownloader/IMGSRC/IMGSRCAlbumParser.cs
@@ -0,0 +1,171 @@
+﻿using HtmlAgilityPack;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace PictureDownloader.IMGSRC
+{
+    public class IMGSRCAlbumParser
+    {
+        public delegate void PicFoundEventHandler();
+
+        public static event PicFoundEventHandler PicFound;
+
+        public static int PicsNum { get; set; }
+
+        public static void GetPics(string albumUrl, string path)
+        {
+            PicsNum = 0;
+            bool isLastPick = false;
+            string currPic = albumUrl;
+            WebBrowser wb = new WebBrowser();
+            do
+            {
+                wb.Navigate(currPic);
+
+                while (wb.ReadyState != WebBrowserReadyState.Complete)
+                {
+                    Application.DoEvents();
+                }
+
+                string albumName = string.Empty;
+                if (PicsNum == 0)
+                {
+                    albumName =  wb.DocumentTitle;
+                }
+
+                HtmlElement opic = wb.Document.GetElementById("oripic");
+                if (opic != null)
+                {
+                    // получаем ссылку на оригинал
+                    string orpic = opic.GetAttribute("href");
+                    DownLoadPic(orpic, albumName, path);
+                    PicsNum++;                    
+                    if (PicFound != null)
+                    {
+                        PicFound();
+                    }
+                }
+                else
+                {
+                    // пытаемся получить ссылку на обычную картинку
+                    HtmlElement pic = wb.Document.GetElementById("bigpic");
+                    if (pic != null)
+                    {
+                        // получаем ссылку на картинку
+                        string upic = pic.GetAttribute("src");
+                        DownLoadPic(upic, albumName, path);
+                        PicsNum++;
+
+                        if (PicFound != null)
+                        {
+                            PicFound();
+                        }
+                    }
+                }
+
+                // получаем ссылку на следующую страницу
+                HtmlElement newpic = wb.Document.GetElementById("new_big_url");
+                if (newpic.GetAttribute("href").IndexOf("/main/user.php?user=") == -1)
+                {
+                    currPic = newpic.GetAttribute("href");
+                }
+                else
+                {
+                    isLastPick = true;
+                }
+
+            }
+            while (!isLastPick);
+        }
+
+        public static void GetPics2(string albumUrl, string path)
+        {
+            PicsNum = 0;
+            bool isLastPick = false;
+            string currPic = albumUrl;
+            WebBrowser wb = new WebBrowser();
+            do
+            {
+                wb.Navigate(currPic);
+
+                while (wb.ReadyState != WebBrowserReadyState.Complete)
+                {
+                    Application.DoEvents();
+                }
+
+                string albumName = string.Empty;
+                if (PicsNum == 0)
+                {
+                    albumName = wb.DocumentTitle;
+                }
+
+                HtmlElement opic = wb.Document.GetElementById("oripic");
+                if (opic != null)
+                {
+                    // получаем ссылку на оригинал
+                    string orpic = opic.GetAttribute("href");
+                    DownLoadPic(orpic, albumName, path);
+                    PicsNum++;
+                    if (PicFound != null)
+                    {
+                        PicFound();
+                    }
+                }
+                else
+                {
+                    // пытаемся получить ссылку на обычную картинку
+                    HtmlElement pic = wb.Document.GetElementById("bigpic");
+                    if (pic != null)
+                    {
+                        // получаем ссылку на картинку
+                        string upic = pic.GetAttribute("src");
+                        DownLoadPic(upic, albumName, path);
+                        PicsNum++;
+
+                        if (PicFound != null)
+                        {
+                            PicFound();
+                        }
+                    }
+                }
+
+                // получаем ссылку на следующую страницу
+                HtmlElement newpic = wb.Document.GetElementById("new_big_url");
+                if (newpic.GetAttribute("href").IndexOf("/main/user.php?user=") == -1)
+                {
+                    currPic = newpic.GetAttribute("href");
+                }
+                else
+                {
+                    isLastPick = true;
+                }
+
+            }
+            while (!isLastPick);
+        }
+
+        private static void DownLoadPic(string pic, string albumName, string path)
+        {
+            WebClient wc = new WebClient();
+            string fileName = pic.Substring(pic.LastIndexOf("/")+1, pic.Length - pic.LastIndexOf("/")-1);
+            //string res = path + "\\" + albumName + "\\" + fileName;
+            string res = path + "\\" + fileName;
+            //res = CleanFileName(res);
+            wc.DownloadFile(pic, res);
+        }
+
+        private static string CleanFileName(string fileName)
+        {
+            return Path.GetInvalidFileNameChars().Aggregate(fileName, (current, c) => current.Replace(c.ToString(), string.Empty));
+        }
+
+
+    }
+}
---
+++ PictureDownloader/MainForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PictureDownloader
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IMGSRC.IMGSRCAlbumParser.PicFound += delegate()
+                {
+                    label1.Text = IMGSRC.IMGSRCAlbumParser.PicsNum.ToString();
+                    Application.DoEvents();
+                };
+            IMGSRC.IMGSRCAlbumParser.GetPics(textBox1.Text, textBox2.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox2.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+    }
+}
